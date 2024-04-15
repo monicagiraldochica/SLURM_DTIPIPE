33e9f7898c1ccdea4b31600145a299215c48c325
monicagiraldochica <monicagiraldochica@gmail.com>
2024-04-15 11:48:08 -0500

    parallel and DB versions

---
+++ 3dmask_DB.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --job-name=3dmask
+#SBATCH --time=00:02:00
+#SBATCH --account=jbinder
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=3gb
+set -e
+STARTTIME=$(date +%s)
+
+module load python/3.9.1
+module load fsl/6.0.4
+PATH=${FSLDIR}/bin:$PATH
+. ${FSLDIR}/etc/fslconf/fsl.sh
+
+sbj=EC1116
+sess=EC1116_1
+scratch=/scratch/g/jbinder/mkeith/ECP
+sessdir=$scratch/$sbj/$sess
+
+echo "Running 3dmask on ${sbj}: ${sess}"
+python3 $scratch/3dmask_DB.py $sess $sessdir
+echo "DONE 3dmask"
+
+FINISHTIME=$(date +%s)
+TOTDURATION_S=$((FINISHTIME - STARTTIME))
+DURATION_H=$((TOTDURATION_S / 3600))
+REMAINDER_S=$((TOTDURATION_S - (3600*DURATION_H)))
+DURATION_M=$((REMAINDER_S / 60))
+DURATION_S=$((REMAINDER_S - (60*DURATION_M)))
+DUR_H=$(printf "%02d" ${DURATION_H})
+DUR_M=$(printf "%02d" ${DURATION_M})
+DUR_S=$(printf "%02d" ${DURATION_S})
+echo -e "\nTotal execution time was ${DUR_H} hrs ${DUR_M} mins ${DUR_S} secs"
