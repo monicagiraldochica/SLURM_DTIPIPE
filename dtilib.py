78fbc42830f61aeb799e4f1defbbf494261cad0f
monicagiraldochica <165931199+monicagiraldochica@users.noreply.github.com>
2026-06-11 12:57:57 -0500

    Allot to check if i process is finished from subprocess.Popen or multiprocessing.process

--- dtilib.py
+++ dtilib.py
@@ -4,8 +4,9 @@ __author__ = "Monica Keith"
 import subprocess
 import os
 import argparse
+import multiprocessing
 
-# force each process to one thread
+# force each process to one thread for a more efficient use of CPUs
 env = os.environ.copy()
 env["OMP_NUM_THREADS"] = "1"
 env["MKL_NUM_THREADS"] = "1"
@@ -14,9 +15,43 @@ env["OPENBLAS_NUM_THREADS"] = "1"
 def runBashCommand(command: list):
     return subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, env=env)
 
+#######
+def runPipeline(commands: list):
+    for command in commands:
+        proc = runBashCommand(command)
+        out, err = proc.communicate()
+        if proc.returncode!=0:
+            print(f"Process failed (PID {proc.pid})\nCommand: {proc.args}\nOutput: {out}\nError: {err}")
+
+def runPipelineParallel(target, *args):
+    p = multiprocessing.Process(target=target, args=args)
+    p.start()
+    return p
+
+def brainExtract2(prefix: str, *, run_all=False, fsl=False, afni=False, freesurfer=False):
+    prefix = prefix.removesuffix(".nii.gz").removesuffix(".nii")
+    proc = extractVolume(prefix, 0)
+    stdout, stderr = proc.communicate()
+    if proc.returncode!=0:
+        raise RuntimeError(f"fslroi failed.\nOutput: {stdout}\nError: {stderr}")
+
+    procs = []
+    if run_all or fsl:
+        procs.append(runBashCommand(["bet", f"{prefix}_b0", f"{prefix}_bet", "-f", "0.1", "-g", "0", "-m"]))
+    if run_all or afni:
+        cmd1 = ["3dSkullStrip", "-input", f"{prefix}_b0", "-prefix", f"{prefix}_sklstrip.nii.gz"]
+        cmd2 = ["3dcalc", "-a", f"{prefix}_sklstrip.nii.gz", "-expr", "step(a)", "-prefix", f"{prefix}_sklstrip_mask.nii.gz"]
+        procs.append(runPipelineParallel(runPipeline,[cmd1, cmd2]))
+    if run_all or freesurfer:
+        procs.append(runBashCommand(["mri_synthstrip", "-i", f"{prefix}_b0.nii.gz", "-o", f"{prefix}_free.nii.gz"]))
+    
+    return procs
+#######
+
 def extractVolume(prefix: str, vol: int):
     return runBashCommand(["fslroi", prefix, f"{prefix}_b0", "0", "-1", "0", "-1", "0", "-1", str(vol), "1"])
 
+# All parameters after * must be passed as keyword arguments
 def brainExtract(prefix: str, *, run_all=False, fsl=False, afni=False, freesurfer=False):
     prefix = prefix.removesuffix(".nii.gz").removesuffix(".nii")
     proc = extractVolume(prefix, 0)
@@ -49,13 +84,19 @@ def read_args():
 
     return parser.parse_args()
 
+def is_finished(p):
+    if isinstance(p, subprocess.Popen):
+        return p.poll() is not None
+    
+    return not p.is_alive()
+
 def throttle(procs, max_procs):
     if max_procs<=0:
         return
     
     while len(procs)>=max_procs:
         for p in procs:
-            if p.poll() is not None:
+            if is_finished(p):
                 stdout, stderr = p.communicate()
                 if p.returncode!=0:
                     print(f"Process failed (PID {p.pid})\nCommand: {p.args}\nOutput: {stdout}\nError: {stderr}")
@@ -80,7 +121,7 @@ def main():
         for brain in brains:
             if not os.path.isfile(brain):
                 continue
-            new_procs = brainExtract(brain,run_all=args.all_soft, fsl=args.fsl, afni=args.afni, freesurfer=args.freesurfer)
+            new_procs = brainExtract(brain, run_all=args.all_soft, fsl=args.fsl, afni=args.afni, freesurfer=args.freesurfer)
 
             for p in new_procs:
                 throttle(procs, args.max_procs)
