ba420fa3951797fc21d6f1782f8b28f5e7b5f8ee
monicagiraldochica <165931199+monicagiraldochica@users.noreply.github.com>
2026-08-01 14:53:03 -0500

    debug: uncomment afni section

--- dtilib.py
+++ dtilib.py
@@ -115,14 +115,14 @@ def brainExtractNIFTI(brain_path: str, *, run_all: bool=False, fsl: bool=False, 
             cmd2 = ["fslmaths", f"{prefix}_bet_mask", "-bin", "-mul", brain_path, f"{orig_prefix}_bet"]
             procs.append(runPipelineParallel(runPipeline, [cmd1, cmd2]))
 
-    #if run_all or afni:
-    #    cmd1 = ["3dSkullStrip", "-overwrite", "-input", f"{prefix}.nii.gz", "-prefix", f"{prefix}_sklstrip.nii.gz"]
-    #    cmd2 = ["3dcalc", "-a", f"{prefix}_sklstrip.nii.gz", "-expr", "step(a)", "-prefix", f"{prefix}_sklstrip_mask.nii.gz"]
-    #    cmd3 = ["3dcalc", "-a", brain_path, "-b", f"{prefix}_sklstrip_mask.nii.gz", "-expr", "step(b)*a", "-prefix", f"{orig_prefix}_sklstrip_mask.nii.gz"]
-    #    if n_vols==1 or skip4Dmasking:
-    #        procs.append(runPipelineParallel(runPipeline, [cmd1, cmd2]))
-    #    else:
-    #        procs.append(runPipelineParallel(runPipeline, [cmd1, cmd2, cmd3]))
+    if run_all or afni:
+        cmd1 = ["3dSkullStrip", "-overwrite", "-input", f"{prefix}.nii.gz", "-prefix", f"{prefix}_sklstrip.nii.gz"]
+        cmd2 = ["3dcalc", "-a", f"{prefix}_sklstrip.nii.gz", "-expr", "step(a)", "-prefix", f"{prefix}_sklstrip_mask.nii.gz"]
+        cmd3 = ["3dcalc", "-a", brain_path, "-b", f"{prefix}_sklstrip_mask.nii.gz", "-expr", "step(b)*a", "-prefix", f"{orig_prefix}_sklstrip_mask.nii.gz"]
+        if n_vols==1 or skip4Dmasking:
+            procs.append(runPipelineParallel(runPipeline, [cmd1, cmd2]))
+        else:
+            procs.append(runPipelineParallel(runPipeline, [cmd1, cmd2, cmd3]))
 
     #if run_all or freesurfer:
     #    cmd1 = ["mri_synthstrip", "-i", f"{prefix}.nii.gz", "-o", f"{prefix}_free.nii.gz", "-m", f"{prefix}_free_mask.nii.gz"]
